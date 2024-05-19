3bb690abad3ac0b63edc421efd70bfd588e7c921
Robinson Freitas <robinsonjunior459@gmail.com>
2024-05-19 10:13:35 -0300

    feat: criação de posts e likes

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -11,7 +11,21 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "coffee", "cyberpunk"],
+    themes: [
+      {
+        vexius: {
+          primary: '#f97316',
+          secondary: '#0ea5e9',
+          accent: '#22d3ee',
+          neutral: '#ff00ff',
+          'base-100': '#111827',
+          info: '#0000ff',
+          success: '#00ff00',
+          warning: '#facc15',
+          error: '#ff0000',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
